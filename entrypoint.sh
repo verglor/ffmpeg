3aadd4c53b4eb1ef4dc3d18d6f5f7dab9c86d68f
Jaroslav Kostal <jaroslav@kostal.sk>
2018-01-16 00:14:59 +0100

    Video codec skip

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 path=${INPUT_PATH}
 mask=${INPUT_REGEX:-"\.(mkv|mp4|avi|ts|flv)$"}
+video_codec_skip=${INPUT_VIDEO_CODEC_SKIP:-hevc}
 ext=${OUTPUT_EXT:-mkv}
 nice=${PROCESS_NICE:-10}
 
@@ -10,14 +11,20 @@ cd /input
 find "./$path" -type f | egrep -i "$mask" | sort | cut -c 3- | while read input
 do
 
+    video_codec=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$input")
+
     echo
     echo "================================================================================"
-    echo ">>> $input"
+    echo ">>> $input [$video_codec]"
     echo "================================================================================"
 
-    output="/output/${input%.*}.$ext"
-    mkdir -p "$(dirname "$output")"
-    nice -n $nice ffmpeg -n -hide_banner -v info -i "$input" "$@" "$output" < /dev/null || true
+    if [ "$video_codec" == "$video_codec_skip" ]; then
+        echo "    *** Video codec is already $video_codec_skip ***"
+    else
+        output="/output/${input%.*}.$ext"
+        mkdir -p "$(dirname "$output")"
+        nice -n $nice ffmpeg -n -hide_banner -v info -i "$input" "$@" "$output" < /dev/null || true
+    fi
 
     echo "--------------------------------------------------------------------------------"
     echo
